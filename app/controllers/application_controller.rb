dc3bb90a2b1c03af57c9ff127eddeac97c1b10d2
su1-90 <sui_face@yahoo.co.jp>
2024-04-10 10:11:52 +0900

    update: ログイン後のパス指定
    ヘルパーメソッド　after_sign_in_path_for
    https://zenn.dev/yuna0960740/articles/dbea6e1ad45b01

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,10 @@ class ApplicationController < ActionController::Base
         devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :profile_image])
         devise_parameter_sanitizer.permit(:account_update, keys: [:profile_image])
     end
+
+    # ログイン後のページ遷移指定
+    def after_sign_in_path_for(resource)
+        chats_path
+    end
     
 end
